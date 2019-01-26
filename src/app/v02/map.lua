0cd8db6995a7cf41b82f12de0f269ee23ec201ce
mayol_l <lucas1.mayol@epitech.eu>
2019-01-26 21:49:02 +0100

    modify map with start weed

--- src/app/v02/map.lua
+++ src/app/v02/map.lua
@@ -1,7 +1,7 @@
 return {
   version = "1.2",
   luaversion = "5.1",
-  tiledversion = "1.2.1",
+  tiledversion = "2018.12.22",
   orientation = "orthogonal",
   renderorder = "right-down",
   width = 50,
@@ -637,6 +637,7 @@ return {
           gid = 1,
           visible = true,
           properties = {
+            ["State"] = 2,
             ["Weed"] = true
           }
         }
